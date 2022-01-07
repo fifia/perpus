32909cec4548dc4c0376cf94044e415f559abd47
sahfira fifia <43955471+fifia@users.noreply.github.com>
2022-01-08 00:38:49 +0700

    footer baru mwehehe

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -1,17 +1,10 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="clearfix"></div>
-<!-- <footer class="main-footer">
-    <div id="mycredit"><strong> Sistem Informasi Perpustakaan SDN 4 Gabus
-    </strong> is another thing from <strong><a href="https://instagram.com/sahfiraefemjee" target="_blank">Sahfira Fifia</a></strong>
-    <div class="pull-right">
-    Halaman dimuat dalam <strong>{elapsed_time}</strong> detik. 
-    </div></div>
-</footer> -->
 <div class="hk-footer-wrap container">
   <footer class="footer">
     <div class="row">
       <div class="col-md-6 col-sm-12">
-        <p>&copy; 2022, Sistem Informasi Perpustakaan SDN 4 Gabus</p>
+        <p>&copy; <?php print date('Y');?><a href="https://instagram.com/sahfiraefemjee" target="_blank">Sahfira Fifia</a>, dibangun dengan ❤️ dan 😭</p>
       </div>
       <div class="col-md-6 col-sm-12">
       <p>Halaman dimuat dalam <strong>{elapsed_time}</strong> detik. </p>
