e8accf3f0d4cc1285ec6b3fa6aa2e07a3699be04
Umar Hadi Siswanto <31447862+umarhadi@users.noreply.github.com>
2022-01-07 18:02:18 +0700

    feat: new footer view

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -1,94 +1,39 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="clearfix"></div>
-<footer class="main-footer">
+<!-- <footer class="main-footer">
     <div id="mycredit"><strong> Sistem Informasi Perpustakaan SDN 4 Gabus
     </strong> is another thing from <strong><a href="https://instagram.com/sahfiraefemjee" target="_blank">Sahfira Fifia</a></strong>
     <div class="pull-right">
     Halaman dimuat dalam <strong>{elapsed_time}</strong> detik. 
     </div></div>
-</footer>
+</footer> -->
+<div class="hk-footer-wrap container">
+  <footer class="footer">
+    <div class="row">
+      <div class="col-md-6 col-sm-12">
+        <p>&copy; 2022, Sistem Informasi Perpustakaan SDN 4 Gabus</p>
+      </div>
+      <div class="col-md-6 col-sm-12">
+      <p>Halaman dimuat dalam <strong>{elapsed_time}</strong> detik. </p>
+      </div>
+    </div>
+  </footer>
+</div>
+<script src="<?= base_url('assets/'); ?>/vendors/jquery/dist/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/vendors/popper.js/dist/umd/popper.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/js/jquery.slimscroll.js"></script>
+<script src="<?= base_url('assets/'); ?>/js/dropdown-bootstrap-extended.js"></script>
+<script src="<?= base_url('assets/'); ?>//js/feather.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/vendors/jquery-toggles/toggles.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/js/toggle-data.js"></script>
+<script src="<?= base_url('assets/'); ?>/vendors/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/vendors/jquery.counterup/jquery.counterup.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/vendors/owl.carousel/dist/owl.carousel.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/vendors/jquery-toast-plugin/dist/jquery.toast.min.js"></script>
+<script src="<?= base_url('assets/'); ?>/js/init.js"></script>
+<script src="<?= base_url('assets/'); ?>/js/dashboard-data.js"></script>
 
-<div id="logout"></div>
-<!-- ./wrapper -->
-<!-- jQuery 3 -->
-<script src="<?php echo base_url();?>assets_style/assets/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?php echo base_url();?>assets_style/assets/bower_components/bootstrap/dist/js/bootstrap.js"></script>
-<script src="<?php echo base_url();?>assets_style/assets/plugins/summernote/summernote-lite.js"></script>
-
-<script>
-    $('#summernotehal').summernote({
-        height: 150,
-        tabsize: 1,
-        direction: 'rtl',
-        toolbar: [
-                ['style', ['style']],
-                ['font', ['bold', 'underline', 'clear']],
-                ['fontname', ['fontname']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-								['view', ['fullscreen', 'help']],
-								['table', ['table']],
-                ],
-	});
-</script>
-<!-- Select2 -->
-<script src="<?php echo base_url();?>assets_style/assets/bower_components/select2/dist/js/select2.full.min.js"></script>
-<script>
-
-$(function() {
-    //Initialize Select2 Elements
-    $('.select2').select2();
-});
-// Restricts input for each element in the set of matched elements to the given inputFilter.
-(function($) {
-  $.fn.inputFilter = function(inputFilter) {
-    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
-      if (inputFilter(this.value)) {
-        this.oldValue = this.value;
-        this.oldSelectionStart = this.selectionStart;
-        this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
-        this.value = this.oldValue;
-        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-      }
-    });
-  };
-}(jQuery));
-// Install input filters.
-$("#uintTextBox").inputFilter(function(value) {
-  return /^\d*$/.test(value); });
-// Install input filters.
-$("#uintTextBox2").inputFilter(function(value) {
-  return /^\d*$/.test(value); });
-$("#uintTextBox3").inputFilter(function(value) {
-  return /^\d*$/.test(value); });
-</script>
-<script>
-    // notifikasi gagal di hide
-    //$("#notifikasi").hide();
-    var logingagal = function(){
-        $("#notifikasi").fadeOut('slow');
-    };
-    setTimeout(logingagal, 4000);
-</script> 
-
-<!-- custom jQuery -->
-<script src="<?php echo base_url();?>assets_style/assets/dist/js/custom.js"></script>
-
-<!-- Logout Ajax -->
-<!-- AdminLTE App -->
-<script src="<?php echo base_url();?>assets_style/assets/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url();?>assets_style/assets/dist/js/demo.js"></script>
-<!-- PACE -->
-<script src="<?php echo base_url();?>assets_style/assets/bower_components/PACE/pace.min.js"></script>
-<!-- DataTables -->
-<script src="<?php echo base_url();?>assets_style/assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="<?php echo base_url();?>assets_style/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<!-- bootstrap datepicker -->
-<script src="<?php echo base_url();?>assets_style/assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- bootstrap time picker -->
-<script src="<?php echo base_url();?>assets_style/assets/plugins/timepicker/bootstrap-timepicker.min.js"></script>
 </body>
+
 </html>
