c9bb2e98c39914e892b1d170d5b3e06e0e85f6d5
Umar Hadi Siswanto <31447862+umarhadi@users.noreply.github.com>
2022-01-09 11:56:22 +0700

    feat: fix button

--- application/views/user/user_view.php
+++ application/views/user/user_view.php
@@ -12,7 +12,7 @@
     <div class="row">
         <div class="col-xl-12">
             <section class="hk-sec-wrapper">
-            <a href="user/tambah" class="btn btn-success btn-wth-icon btn-rounded text-white icon-left mb-30"><span class="btn-text">Tambah Pengguna</span> <span class="icon-label"><span class="feather-icon"><i data-feather="user-plus"></i></span></span></a>
+                <a href="user/tambah" class="btn btn-success btn-wth-icon btn-rounded text-white icon-left mb-30"><span class="btn-text">Tambah Pengguna</span> <span class="icon-label"><span class="feather-icon"><i data-feather="user-plus"></i></span></span></a>
                 <div class="row">
                     <div class="col-lg">
                         <div class="table-wrap">
@@ -51,11 +51,10 @@
                                             <td><?= $isi['jenkel']; ?></td>
                                             <td><?= $isi['level']; ?></td>
                                             <td>
-                                                <a href="<?= base_url('user/edit/' . $isi['id_login']); ?>"><button class="btn btn-success"><i class="fa fa-edit"></i></button></a>
-                                                <a href="<?= base_url('user/del/' . $isi['id_login']); ?>" onclick="return confirm('Anda yakin user akan dihapus ?');">
-                                                    <button class="btn btn-danger"><i class="fa fa-trash"></i></button></a>
-                                                <a href="<?= base_url('user/detail/' . $isi['id_login']); ?>" target="_blank"><button class="btn btn-primary">
-                                                        <i class="fa fa-print"></i> Cetak Kartu</button></a>
+                                                <a href="<?= base_url('user/edit/' . $isi['id_login']); ?>"><button class="btn btn-icon btn-icon-circle btn-success btn-icon-style-2"><span class="btn-icon-wrap"><i class="fa fa-pencil"></i></span></button></a>
+                                                <a href="<?= base_url('user/del/' . $isi['id_login']); ?>" onclick="return confirm('Hapus pengguna dengan username <?= $isi['user']; ?>?');">
+                                                    <button class="btn btn-icon btn-icon-circle btn-danger btn-icon-style-2"><span class="btn-icon-wrap"><i class="icon-trash"></i></span></button></a>
+                                                <a href="<?= base_url('user/detail/' . $isi['id_login']); ?>" target="_blank"><button class="btn btn-warning btn-wth-icon btn-md btn-rounded"> <span class="icon-label"><span class="feather-icon"><i data-feather="printer"></i></span> </span><span class="btn-text">Cetak Kartu</span></button></a>
                                             </td>
                                         </tr>
                                     <?php $no++;
